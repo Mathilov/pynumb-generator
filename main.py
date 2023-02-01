d1a6a420352fa12e01b20c2880461925986cc31a
mathiasovreseth <stryn099@hotmail.no>
2023-02-01 10:08:43 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    for i in range(10):
+        print(random.randint(1, 100))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
