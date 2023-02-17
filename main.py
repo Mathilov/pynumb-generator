1186812e39facb28cbd145eeebab4b5b5c1d3bee
mathiasovreseth <stryn099@hotmail.no>
2023-02-17 12:34:27 +0100

    Changes to program to cause it to loop

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import random
+import time
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
-
+start_time = time.time()
+end_time = start_time + 5 * 60  # 5 minutes in seconds
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    while time.time() < end_time:
+        # generate and print a random number
+        random_number = random.randint(1, 100)
+        logging.info(f"Printing number {random_number}")
 
-    for i in range(10):
-        print(random.randint(1, 100))
+        # add a 5 second delay
+        time.sleep(10)
+
+    logging.info("Application finished")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
