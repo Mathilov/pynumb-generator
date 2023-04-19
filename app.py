70a2bef034ba6ba37f74a9eb6f0892761ada9d3c
Mathias <stryn099@hotmail.no>
2023-04-19 12:54:27 +0200

    added more comements

--- app.py
+++ app.py
@@ -4,18 +4,12 @@ import random
 import redis
 import logging
 
-from azure.core.exceptions import ResourceExistsError, ResourceNotFoundError
-from azure.storage.blob import BlobClient, BlobServiceClient
+from azure.storage.blob import BlobServiceClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
-
-# constructs a connection string for the Azure Blob Storage.
-storage_account_name = os.getenv('storage_account_name')
-storage_account_key = os.getenv('storage_account_key')
-connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={storage_account_key};EndpointSuffix=core.windows.net"
 
 
 # Retrieves the Redis server host and port from the environment variables or set default values if they are not provided.
@@ -23,17 +17,29 @@ redis_host = os.environ.get("REDIS_HOST", "redis")
 redis_port = int(os.environ.get("REDIS_PORT", 6379))
 
 # Creates a Redis client instance named cache using the host, port, and database number.
-cache = redis.Redis(host=redis_host, port=redis_port, db=0)
+cache = redis.Redis(host=redis_host, port=redis_port, db=0, password='testPassord')
 
+# constructs a connection string for the Azure Blob Storage.
+storage_account_name = os.getenv('storage_account_name')
+storage_account_key = os.getenv('storage_account_key')
+connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={storage_account_key};EndpointSuffix=core.windows.net"
+
+
+# creates an instance of the BlobServiceClient
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
 container_name = "mycontainer"
+
+# creates an instance of the ContainerClient
 container_client = blob_service_client.get_container_client(container_name)
 
 # Create the root container if it doesn't already exist
 if not container_client.exists():
     container_client.create_container()
+
+
 blob_name = "numbers.txt"
+# Returns a new instance of the BlobClient class for the specified blob
 blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
 
 # write to redis instance
@@ -46,16 +52,17 @@ def write_random_numbers_to_redis():
 
 #write to blob
 def write_random_numbers_to_blob():
-    # creates a client instance
 
 
     while True:
         random_number = random.randint(1, 100)
+        # Check if the blob exists, if so download the data
         if blob_client.exists():
+            # Get the current contents of the blob
             current_content = blob_client.download_blob().content_as_text()
         else:
             current_content = ""
-        # Get the current contents of the blob
+
 
         # Append the new random number to the current contents
         data = f"{current_content}\n{random_number}"
@@ -66,4 +73,4 @@ def write_random_numbers_to_blob():
 
 
 if __name__ == "__main__":
-    write_random_numbers_to_blob()
+    write_random_numbers_to_redis()
--- dockerfile
+++ dockerfile
@@ -2,9 +2,20 @@ FROM python:3.9-slim
 
 WORKDIR /app
 
+# Create and activate virtual environment
+RUN python -m venv /opt/venv
+ENV PATH="/opt/venv/bin:$PATH"
+RUN python -m pip install --upgrade pip
+
+
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
-COPY app.py app.py
+# Install python-dotenv package
+RUN pip install python-dotenv
 
-CMD ["python", "app.py"]
+COPY . .
+
+CMD [ "python", "-c", "from dotenv import load_dotenv; load_dotenv()" ]
+CMD [ "python", "app.py" ]
+
