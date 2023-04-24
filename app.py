464d98ae63e52ec54da1e2f07e02c88892273da7
Mathias <stryn099@hotmail.no>
2023-04-24 12:19:03 +0200

    env fetching for redis port fix

--- app.py
+++ app.py
@@ -11,26 +11,17 @@ load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 
-
-
-
 # constructs a connection string for the Azure Blob Storage.
 storage_account_name = os.getenv('storage_account_name')
 storage_account_key = os.getenv('storage_account_key')
 connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={storage_account_key};EndpointSuffix=core.windows.net"
 
 
-
-
-
-
-
-
 # write to redis instance
 def write_random_numbers_to_redis():
     # Retrieves the Redis server host and port from the environment variables or set default values if they are not provided.
     redis_host = os.environ.get("REDIS_HOST", "redis")
-    redis_port = os.environ.get("REDIS_PORT", 6379)
+    redis_port = int(os.environ.get("REDIS_PORT", 6379))
 
     # Creates a Redis client instance named cache using the host, port, and database number.
     cache = redis.Redis(host=redis_host, port=redis_port, db=0)
@@ -40,7 +31,8 @@ def write_random_numbers_to_redis():
         logging.info(f"Wrote {random_number} to Redis")
         time.sleep(10)
 
-#write to blob
+
+# write to blob
 def write_random_numbers_to_blob():
     # creates an instance of the BlobServiceClient
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
@@ -52,6 +44,7 @@ def write_random_numbers_to_blob():
     # Create the root container if it doesn't already exist
     if not container_client.exists():
         container_client.create_container()
+
     blob_name = "numbers.txt"
     # Returns a new instance of the BlobClient class for the specified blob
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
@@ -65,13 +58,11 @@ def write_random_numbers_to_blob():
         else:
             current_content = ""
 
-
         # Append the new random number to the current contents
         data = f"{current_content}\n{random_number}"
         blob_client.upload_blob(data, blob_type="AppendBlob", overwrite=True)
         logging.info(f"Wrote {data} to blob storage")
         time.sleep(10)
-
 
 
 if __name__ == "__main__":
